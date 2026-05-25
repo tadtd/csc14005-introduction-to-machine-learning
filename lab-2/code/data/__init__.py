60409af66b59fa79bdab1e96970e35b330816c0f
tadtd <tiendat1243oo@gmail.com>
2026-05-25 15:10:36 +0700

    feat: update data loading scripts for automatic dataset downloads

--- lab-2/code/data/__init__.py
+++ lab-2/code/data/__init__.py
@@ -3,27 +3,19 @@
 from .load_data import (
     ALL_DATASET_KEYS,
     DatasetBundle,
-    load_20newsgroups_tfidf,
     load_coil20,
     load_dataset,
-    load_iris,
     load_mnist,
-    load_pbmc3k,
     load_swiss_roll,
-    load_wine,
     make_circles_,
 )
 
 __all__ = [
     "ALL_DATASET_KEYS",
     "DatasetBundle",
-    "load_20newsgroups_tfidf",
     "load_coil20",
     "load_dataset",
-    "load_iris",
     "load_mnist",
-    "load_pbmc3k",
     "load_swiss_roll",
-    "load_wine",
     "make_circles_",
 ]
